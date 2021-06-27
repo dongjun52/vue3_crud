5fc912e71ca2f61c99bb6092c6d00dd6fe548c6e
Andy <ldj5471@gmail.com>
2021-06-27 12:54:19 +0900

    test commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <andyMall />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import andyMall from "@/components/andyMall.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    andyMall,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/andyMall.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>test</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "andyMall",
+  components: {},
+});
+</script>
+
+<style></style>
